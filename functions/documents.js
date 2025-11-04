f9b406728e945eb834b0600f45497f1ff134b3e2
flokor99 <FKorte@stroeer.de>
2025-11-04 17:23:40 +0100

    Update documents.js

--- functions/documents.js
+++ functions/documents.js
@@ -1,9 +1,11 @@
 import { blobs } from '@netlify/blobs'
 
-export async function handler() {
+export const handler = async () => {
   const idx = (await blobs.getJSON('docs/index.json')) || { docIds: [] }
   const docs = await Promise.all(idx.docIds.map(id => blobs.getJSON(`docs/${id}.json`)))
-  return new Response(JSON.stringify(docs.filter(Boolean)), {
-    headers: { 'Content-Type': 'application/json' }
-  })
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(docs.filter(Boolean)),
+  }
 }
