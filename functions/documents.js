0bf93a1a0e606a0747dca6503573e98ecc555e11
flokor99 <FKorte@stroeer.de>
2025-11-04 17:09:35 +0100

    Update documents.js

--- functions/documents.js
+++ functions/documents.js
@@ -1,6 +1,6 @@
-import { blobs } from '@netlify/blobs'
-
-export const handler = async () => {
+// funktioniert auch, wenn die Function als CommonJS läuft
+exports.handler = async () => {
+  const { blobs } = await import('@netlify/blobs') // dynamischer Import von ESM
   const idx = (await blobs.getJSON('docs/index.json')) || { docIds: [] }
   const docs = await Promise.all(idx.docIds.map(id => blobs.getJSON(`docs/${id}.json`)))
   return {
