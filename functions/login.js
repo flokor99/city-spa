a2acb2bd3b7a4937120fdf941292c6faa54e79ed
flokor99 <FKorte@stroeer.de>
2025-11-27 12:54:34 +0100

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -1,11 +1,12 @@
 exports.handler = async (event) => {
-  const { password } = JSON.parse(event.body || '{}')
+  const { password } = JSON.parse(event.body || "{}")
 
   if (password === process.env.SITE_PASSWORD) {
     return {
       statusCode: 200,
       headers: {
-        "Set-Cookie": `session=valid; HttpOnly; Path=/; Max-Age=86400`
+        // HttpOnly raus, damit React das Cookie sehen kann
+        "Set-Cookie": "session=valid; Path=/; Max-Age=86400; SameSite=Lax"
       },
       body: JSON.stringify({ ok: true })
     }
