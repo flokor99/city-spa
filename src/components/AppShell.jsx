835cde53be6ccea7d17b2751cc60276862ee50f6
flokor99 <FKorte@stroeer.de>
2025-12-01 10:39:59 +0100

    Update AppShell.jsx

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -67,7 +67,7 @@ export default function AppShell({ title, children }) {
     style={{
       position: "absolute",
       right: "215px",   // Skyline endet leicht links vom Text "Dokumente"
-      bottom: "-18px",  // untere Kante genau am orangenen Strich
+      bottom: "-21px",  // untere Kante genau am orangenen Strich
       height: "58px",
       opacity: 0.9,
       zIndex: 1,
